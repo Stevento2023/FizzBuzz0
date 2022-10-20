ef82dabce5ebfbffc8f26d5a3c846c4022c5cf75
Steven To <steven.to@mrjaffesclass.com>
2022-10-20 11:12:22 -0700

    continued

--- FizzBuzz0/FizBuzz0.java
+++ FizzBuzz0/FizBuzz0.java
@@ -1,19 +1,32 @@
 public class FizBuzz0
 {
     int i = 0;
-    int a = 3;
-    
+    int a = 0;
+    int b = 0;
     
     public void run() {
         for (int i=0; i <= 100; i=i+1) {
             System.out.println(i);
-            if (i==a+3) {
+            if (i==a) {
                 System.out.println("Fizz");
+            }
+            if (i==b) {
+                System.out.println("Buzz");
             }
             else {
                 System.out.println(i);
             }
         }
+        
+        a = 0;
+        do {
+            a=a+3;
+        } while (a <= 100);
+        
+        b = 0;
+        do {
+            b=b+5;
+        } while (b <= 100);
     }
     
     public static void main(String[] args)
